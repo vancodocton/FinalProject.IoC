2a811e48b76c9e2b2443c174d895b54fe0876032
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-11-18 23:21:56 +0700

    Fix formatting

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ data "azurerm_resource_group" "rg_main" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "main" {
-  name                        = "FinalProjectKeyVault"
-  location                    = data.azurerm_resource_group.rg_main.location
-  resource_group_name         = data.azurerm_resource_group.rg_main.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  sku_name                    = "standard"
+  name                       = "FinalProjectKeyVault"
+  location                   = data.azurerm_resource_group.rg_main.location
+  resource_group_name        = data.azurerm_resource_group.rg_main.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  sku_name                   = "standard"
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
