5fcf52531d3942b877e67854eb5c0eeeb20b3512
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-11-19 11:26:45 +0700

    Use azurerm_key_vault_access_policy
    Allow set multiple records of access policy for Key Vault

--- main.tf
+++ main.tf
@@ -11,20 +11,17 @@ resource "azurerm_key_vault" "main" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
   sku_name                   = "standard"
+}
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Set",
-      "Get",
-      "List",
-      "Delete",
-      "Purge",
-      "Recover"
-    ]
-  }
+resource "azurerm_key_vault_access_policy" "current" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+  secret_permissions = [
+    "Set",
+    "Get",
+    "Delete",
+  ]
 }
 
 resource "azurerm_postgresql_flexible_server" "main" {
