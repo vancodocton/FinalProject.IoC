0f83ce8ed15c2d7c452bc98b848eb25fb0536786
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-11-20 13:21:43 +0700

    Fix formatting of conn str

--- vault.tf
+++ vault.tf
@@ -22,7 +22,7 @@ resource "azurerm_key_vault_secret" "postgres_server_admin_password" {
 
 resource "azurerm_key_vault_secret" "postgres_identity_db_dotnet_connection_string" {
   name         = "identity-db-dotnet-connection-string"
-  value        = "Server=${azurerm_postgresql_flexible_server.main.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.identity.name};Port=5432;UID=${azurerm_key_vault_secret.postgres_server_admin_login.value};;Password=${azurerm_key_vault_secret.postgres_server_admin_password.value}"
+  value        = "Server=${azurerm_postgresql_flexible_server.main.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.identity.name};Port=5432;UID=${azurerm_key_vault_secret.postgres_server_admin_login.value};Password=${azurerm_key_vault_secret.postgres_server_admin_password.value};"
   key_vault_id = azurerm_key_vault.main.id
   depends_on = [
     azurerm_key_vault_access_policy.current
