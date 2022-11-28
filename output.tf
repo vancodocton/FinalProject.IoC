2579c1915ad6def529efbfdad4e028a52756b5f4
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-11-29 03:14:42 +0700

    Update output.tf
    time_rotating_date must be the rotation timestamp, not the base timestamp

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "time_rotating_date" {
-  value       = time_rotating.keyvault_secrets_rotation.id
+  value       = time_rotating.keyvault_secrets_rotation.rotation_rfc3339
   description = "The date that the secrets of the key vault should be renewed."
 }
