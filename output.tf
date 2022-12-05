17964b98a9211a93c6d62b570c6eb5c7c425fa42
Nguyen Ky Duong Truong <duongtruong.nguyenky@gmail.com>
2022-12-05 09:13:49 +0700

    Update output

--- output.tf
+++ output.tf
@@ -2,3 +2,11 @@ output "time_rotating_date" {
   value       = time_rotating.keyvault_secrets_rotation.rotation_rfc3339
   description = "The date that the secrets of the key vault should be renewed."
 }
+
+output "idsv" {
+  value = {
+    web_app_name        = azurerm_linux_web_app.identity.name,
+    resource_group_name = azurerm_linux_web_app.identity.resource_group_name,
+  }
+  sensitive = true
+}
