78dca78321fb003f9082429f04a7d8939456096f
Nguyen Ky Duong Truong <duongtruong.nguyenky@gmail.com>
2022-12-07 16:35:01 +0700

    Using azurerm backend

--- modules/azurerm_source_control/provider.tf
+++ modules/azurerm_source_control/provider.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+    container_name       = "tfstate"
+    storage_account_name = "finalprojecttfstate"
+    key                  = "COMP1682.arm_source_control.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
