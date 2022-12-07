a130e9dfb7192accda6050f82f5f35993b42f9ee
Nguyen Ky Duong Truong <duongtruong.nguyenky@gmail.com>
2022-12-07 17:04:10 +0700

    Use data source azurerm_resource_group

--- modules/azurerm_source_control/main.tf
+++ modules/azurerm_source_control/main.tf
@@ -1,6 +1,10 @@
+data "azurerm_resource_group" "main" {
+  name = var.resource_group_name
+}
+
 data "azurerm_linux_web_app" "main" {
   name                = var.web_app_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
 resource "azurerm_app_service_source_control" "main" {
