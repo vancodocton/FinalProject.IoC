8cdd34171046b8cdc0f7c76a579e8ad57f163629
Nguyen Ky Duong Truong <duongtruong.nguyenky@gmail.com>
2022-12-11 11:10:42 +0700

    chore: update script IO

--- scripts/azure-rbac.ps1
+++ scripts/azure-rbac.ps1
@@ -1,14 +1,16 @@
-# $env:resource_group_name="null"
 # $env:ARM_SP_NAME="SP_FE"
 
 $env:ARM_RESOURCE_GROUP_ID = (az group show --name $env:resource_group_name --query id)
 
-az ad sp create-for-rbac --name $env:ARM_SP_NAME --role contributor `
+$credentials = az ad sp create-for-rbac --name $env:ARM_SP_NAME --role contributor `
     --scopes $env:ARM_RESOURCE_GROUP_ID `
+    --sdk-auth
+Write-Output $credentials;
+$credentialsObject = $credentials | ConvertFrom-Json
 
-$env:ARM_SUBSCRIPTION_ID = "null"
-$env:ARM_CLIENT_ID = "null"
-$env:ARM_TENANT_ID = "null"
-$env:ARM_CLIENT_SECRET = "null"
+$env:ARM_SUBSCRIPTION_ID = $credentialsObject.subscriptionId
+$env:ARM_CLIENT_ID = $credentialsObject.clientId
+$env:ARM_TENANT_ID = $credentialsObject.tenantId
+$env:ARM_CLIENT_SECRET = $credentialsObject.clientSecret
 
 az login --service-principal -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET --tenant $env:ARM_TENANT_ID
--- scripts/backend.ps1
+++ scripts/backend.ps1
@@ -1,9 +1,9 @@
-# $RESOURCE_GROUP_NAME="null"
-# $STORAGE_ACCOUNT_NAME="null"
-# $CONTAINER_NAME="null"
+# $env:CONTAINER_NAME="null"
+# $env:resource_group_location = "null"
+# $env:resource_group_name = "null"
 
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $env:resource_group_name --name $env:STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $env:CONTAINER_NAME --account-name $env:STORAGE_ACCOUNT_NAME
 
-(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+(az storage account keys list --resource-group $env:resource_group_name --account-name $env:STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
